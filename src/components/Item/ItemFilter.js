68bcd7ad6d270bf00ffe0dd225f7c2bdb128066e
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-18 17:48:50 +0900

    필터 옵션 추가

--- src/components/Item/ItemFilter.js
+++ src/components/Item/ItemFilter.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 const ItemFilter = () => {
+    
     return (
         <div>
             <select name='typeFilter'>
                 <option>유형 필터</option>
+                <option>식료품</option>
+                <option>의류</option>
+                <option>교통비</option>
+                <option>건강</option>
+                <option>여가비</option>
             </select>
             <select name='orderFilter'>
                 <option>정렬 기준</option>
+                <option>가격 높은 순</option>
+                <option>가격 낮은 순</option>
+                <option>최신 순</option>
+                <option>오래된 순</option>
             </select>
             <input type='date' />
             <input type='date' />
